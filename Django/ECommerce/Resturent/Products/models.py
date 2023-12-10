b48984d82407daa8d754edce3a3a45f4c530d029
PoornimaPDT <147930393+PoornimaPDT@users.noreply.github.com>
2023-12-10 20:22:30 +0530

    update files

--- Django/ECommerce/Resturent/Products/models.py
+++ Django/ECommerce/Resturent/Products/models.py
@@ -18,3 +18,19 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s {self.product.name} ({self.quantity})"
+    
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_id = models.AutoField(primary_key=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order ID: {self.order_id}, User: {self.user.username}, Total Amount: {self.total_amount}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"Order ID: {self.order.order_id}, Item: {self.product.name}, Quantity: {self.quantity}"
